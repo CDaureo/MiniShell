92bc05aa90b8e2314c67ddbf4def83a98b56e550
CDaureo <cdaureo08@gmail.com>
2025-06-10 16:59:17 +0200

    Pwd on minishell

--- src/builds/handle_builds.c
+++ src/builds/handle_builds.c
@@ -6,7 +6,7 @@
 /*   By: cdaureo- <cdaureo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 19:41:47 by cdaureo-          #+#    #+#             */
-/*   Updated: 2025/06/10 16:28:46 by cdaureo-         ###   ########.fr       */
+/*   Updated: 2025/06/10 16:40:55 by cdaureo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	handle_builds(char **argv, t_ms *ms)
 
 	if (ft_strcmp(argv[0], "cd") == 0)
 		return(ms->exit_status = ft_cd(argv, ms->envp), 1);
+	else if (ft_strcmp(argv[0], "pwd") == 0)
+	{
+		/* code */
+	}
+	
 	
 	return(0);
 }
--- src/main.c
+++ src/main.c
@@ -6,11 +6,17 @@
 /*   By: cdaureo- <cdaureo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:39:32 by cdaureo-          #+#    #+#             */
-/*   Updated: 2025/06/04 20:10:42 by cdaureo-         ###   ########.fr       */
+/*   Updated: 2025/06/10 16:49:36 by cdaureo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include <unistd.h>   // getcwd, gethostname
+#include <pwd.h>      // getpwuid
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv, char **envp)
 {
@@ -18,27 +24,75 @@ int main(int argc, char **argv, char **envp)
     (void)argv;
     char *line;
     t_token *tokens;
-	t_ms ms;
-	ms.envp = copy_envp(envp);
+    t_ms ms;
+    ms.envp = copy_envp(envp);
 
-    while ((line = readline("minishell>>>> ")))
+    char cwd[4096];
+    char hostname[256];
+    char *prompt;
+    char *user;
+    struct passwd *pw;
+
+    while (1)
     {
+		pw = getpwuid(getuid());
+		if (pw && pw->pw_name)
+		    user = pw->pw_name;
+		else
+		    user = "user";
+
+        if (gethostname(hostname, sizeof(hostname)) != 0)
+            strcpy(hostname, "minishell");
+
+        if (!getcwd(cwd, sizeof(cwd)))
+            strcpy(cwd, "~");
+
+        // Obtener $HOME
+        char *home = getenv("HOME");
+        char *display_cwd = cwd;
+
+        // Si cwd empieza por $HOME, reemplaza por ~
+        if (home && strncmp(cwd, home, strlen(home)) == 0)
+        {
+            // +1 para el slash final, si quieres ~/ en vez de solo ~
+            size_t home_len = strlen(home);
+            size_t new_len = strlen(cwd) - home_len + 2;
+            char *tmp = malloc(new_len);
+            if (tmp)
+            {
+                tmp[0] = '~';
+                strcpy(tmp + 1, cwd + home_len);
+                display_cwd = tmp;
+            }
+        }
+
+        size_t prompt_len = strlen(user) + 1 + strlen(hostname) + 1 + strlen(display_cwd) + 3;
+        prompt = malloc(prompt_len);
+        if (!prompt)
+            prompt = strdup("$ ");
+        else
+            snprintf(prompt, prompt_len, "%s@%s:%s$ ", user, hostname, display_cwd);
+
+        line = readline(prompt);
+        free(prompt);
+        if (display_cwd != cwd)
+            free(display_cwd);
+        if (!line)
+            break;
         if (*line)
         {
             add_history(line);
-            tokens = lexer(line); // Usa tu lexer de token_helpers.c
+            tokens = lexer(line);
             if (tokens)
             {
-				char **argv_cmd = tokens_to_str(tokens); // Convierte tokens a argv
-
-				if (!handle_builds(argv_cmd, &ms))
-                execute_pipeline(tokens, envp); // Usa tu función de pipex.c
-			ft_free_split(argv_cmd);
-			free_tokens(tokens);            // Libera memoria de tokens
+                char **argv_cmd = tokens_to_str(tokens);
+                if (!handle_builds(argv_cmd, &ms))
+                    execute_pipeline(tokens, envp);
+                ft_free_split(argv_cmd);
+                free_tokens(tokens);
             }
         }
         free(line);
     }
     return 0;
 }
- 
